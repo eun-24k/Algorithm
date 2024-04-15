3742998ef9c0e7eb41142b36ecdb4b9c7e9649a0
Euny Kim <euny4535@gmail.com>
2024-04-15 23:07:03 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120956?language=kotlin
    문제를 제대로 읽어야한다. 진짜 코드카타 할 때마다 이 생각을 하는 것 같은데 꼼꼼하게 읽는게 정말 나한테는 힘든 것 같다. 그래도 계속 의식하면서 개선해 나가야 할 부분인 것 같다. 항상 막혔다가 문제 정독하고 20분 내로 문제가 해결되는 것 같아서 답답하당..
    
    풀이 방법 :
    아야, 예, 우, 마로 만들수 있는 모든 단어의 경우의 수를 찾아주는 함수를 만들어 주었다. 단어들이 중복되어 저장되지 않게 set에다가 담아주었고 set이랑 중복된 babbling이 있는지 확인하는 방식으로 진행을 해주었다.
    
    조금 더 효율적으로 풀어나갈 수 있는 방법이 있을 지 추후에 고민해 보면 좋을 것 같다는 생각이 들었다.

--- in progress/120956.kt
+++ in progress/120956.kt
@@ -1,1 +1,39 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120956?language=kotlin
+
+class Solution {
+    fun solution(babbling: Array<String>): Int {
+        var answer: Int = 0
+        var babbles = getAllPosibilities()
+        
+        for (i in babbling.indices) {
+            if (babbling[i] in babbles) {
+                answer += 1
+            }
+        }
+        return answer
+    }
+    
+    fun getAllPosibilities(): Set<String> {
+        var words = arrayOf("aya", "ye", "woo", "ma")
+        var allPosibilities = mutableSetOf<String>()
+        for (i in words.indices) {
+            allPosibilities += words[i]
+            for (j in words.indices) {
+                if (i != j) {
+                    allPosibilities += words[i]+words[j]
+                }
+                for (k in words.indices) {
+                    if (i!=j && j!=k) {
+                        allPosibilities += words[i] + words[j] + words[k]
+                    }
+                    for (l in words.indices) {
+                        if (i!=j && j!=k && k!=l) {
+                            allPosibilities += words[i] + words[j] + words[k] +words[l]
+                        }
+                    }
+                }
+            }
+        }
+        return allPosibilities
+    }
+}
