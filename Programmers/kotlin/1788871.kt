488da0d39398635da11eb5245cee884e3a905ba7
Euny Kim <euny4535@gmail.com>
2024-05-15 15:50:50 +0900

    level 0. Completed (2minute)

--- Programmers/kotlin/1788871.kt
+++ Programmers/kotlin/1788871.kt
@@ -13,3 +13,25 @@ class Solution {
         return answer
     }
 }
+
+// solution I found
+
+class Solution {
+    fun solution(players: Array<String>, callings: Array<String>): Array<String> {
+        var nameToRank: MutableMap<String, Int> = hashMapOf()
+        players.forEachIndexed { index, player -> nameToRank[player] = index }
+        callings.forEach {
+            player -> var findIndex = nameToRank[player]!!
+            var switchedPlayer = players[findIndex - 1]
+
+            //원래 배열 자리바꾸기
+            players[findIndex] = switchedPlayer
+            players[findIndex - 1] = player
+
+            //Map update
+            nameToRank[player] = nameToRank[player]!! - 1
+            nameToRank[switchedPlayer] = nameToRank[switchedPlayer]!! + 1
+        }
+        return players
+    }
+}
